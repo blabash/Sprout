5204694eb9e6ccb403d52fc7e9cecee531c79d2b
brett <brett.labash@gmail.com>
2019-04-30 17:36:02 -0700

    Add a bunch of user auth stuff and working on sessionsErrorReducer

---
+++ frontend/sprout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { login, logout, signup } from "./util/session_api_util"; //testing
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.login = login;
+    window.logout = logout;
+    window.signup = signup;
+    
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Welcome to Sprout</h1>, root);
+});
---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,24 @@
+export const login = user => {
+    return(
+        $.ajax({
+            method: 'post',
+            url: '/api/session',
+            data: {user}
+        })
+    )
+};
+
+export const signup = user => (
+    $.ajax({
+        method: 'post',
+        url: '/api/user',
+        data: {user}
+    })
+);
+
+export const logout = () => (
+    $.ajax({
+        method: 'delete',
+        url: '/api/session'
+    })
+);
