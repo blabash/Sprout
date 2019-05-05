5fc27e543d9c8fcfdd2aa9df4fa175e7ede45547
Bretty Labash <brett.labash@gmail.com>
2019-05-05 11:43:31 -0700

    Initial post styling

--- frontend/components/dashboard/post_feed_item.jsx
+++ frontend/components/dashboard/post_feed_item.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const PostFeedItem = (props) => {
     return (
     	<div className="feed-item">
+    	  <div className="feed-item-avatar-image">
+    	     <img src="https://assets.tumblr.com/images/default_avatar/octahedron_closed_64.png" 
+    	     height="64" width="64"/>
+    	  </div>
           <div className="feed-item-title">{props.post.title}</div>
           <div className="feed-item-body">{props.post.body}</div>
           {props.post.post_type}
