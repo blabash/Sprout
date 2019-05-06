11ddaf7d303ae67ee385dca34402646681b10575
Bretty Labash <brett.labash@gmail.com>
2019-05-06 11:44:16 -0700

    Post form styling

--- frontend/components/dashboard/postForms/text_post_form.jsx
+++ frontend/components/dashboard/postForms/text_post_form.jsx
@@ -35,18 +35,26 @@ class TextPostForm extends React.Component {
                     {this.props.currentUser.username}
                 </button>
                 <form className="post-form-form" onSubmit={this.handleSubmit}>
+                    <div className="post-form-inputs">
+                    <div className="post-form-title">
                     <input
                     type="text"
+                    className="post-form-input"
                     value={this.state.title}
                     onChange={this.update("title")}
                     placeholder="Title"
                     />
+                    </div>
+                    <div className="post-form-body">
                     <input
                     type="text"
+                    className="post-form-input"
                     value={this.state.body}
                     onChange={this.update("body")}
                     placeholder="Your text here."
                     />
+                    </div>
+                    </div>
                     <input className="post-form-form-post-button" 
                         type="submit"
                         value="Post"/>
