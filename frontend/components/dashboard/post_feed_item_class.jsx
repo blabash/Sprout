89e5fb1218b5092831dd423cb82992db875eff10
Bretty Labash <brett.labash@gmail.com>
2019-05-09 01:12:11 -0700

    Moved background.css content to api, size change on post_bar, start of styling for new photo post

--- frontend/components/dashboard/post_feed_item_class.jsx
+++ frontend/components/dashboard/post_feed_item_class.jsx
@@ -21,16 +21,16 @@ class PostFeedItemClass extends React.Component {
 
         if (likeUserIds.includes(this.props.currentUserId)) {
             return (
-                <button className="feed-item-unlike-button"
+                <button className="feed-item-icon-button red"
                     onClick={() => this.props.unlike(this.props.post.id)}>
-                    unlike
+                    <i className="icon_like"/>
         </button>
             )
         } else {
             return (
-                <button className="feed-item-like-button"
+                <button className="feed-item-icon-button"
                     onClick={() => this.props.like(this.props.currentUserId, this.props.post.id)}>
-                    like
+                    <i className="icon_like"/>
         </button>
             )
         }
@@ -48,11 +48,11 @@ class PostFeedItemClass extends React.Component {
         if (this.props.post.user_id === this.props.currentUserId) {
             return (
                 <div className="post-feed-item-buttons">
-                    <button className="post-edit-button">Edit</button>
+                    <button className="feed-item-icon-button"><i className="icon_edit"/></button>
 
                     <button onClick={() => this.props.deletePost(this.props.post.id)} 
-                            className="post-delete-button">
-                            Delete
+                            className="feed-item-icon-button">
+                            <i className="icon_delete"/>
                     </button>
                 </div>
             )
@@ -70,11 +70,12 @@ class PostFeedItemClass extends React.Component {
     render() {
         return (
             <div className="feed-item">
+              <div className="feed-item-avatar">
+                <img src={this.props.postOwner.profile_pic} className="feed-item-avatar-image" height="64" width="64" />
+              </div>
+              <div className="feed-content">
                 <div className="post-feed-post-owner-username">
                     {this.props.postOwner.username}
-                </div>
-                <div className="feed-item-avatar-image">
-                    <img src={this.props.postOwner.profile_pic} height="64" width="64" />
                 </div>
                 <div className="feed-item-title">{this.props.post.title}</div>
                 <div className="photo-post-img">
@@ -88,7 +89,11 @@ class PostFeedItemClass extends React.Component {
                 <div className="feed-item-like-unlike-buttons">
                     {this.isLikedByCurrentUser()}
                 </div>
+                <div className="feed-item-icon-button">
+                    <i className="icon_reblog"/>
+                </div>
                 {this.displayInteractionButtons()}
+              </div>
             </div>
         )
     }
