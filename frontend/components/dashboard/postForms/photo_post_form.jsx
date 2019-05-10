0417ba3a62f024fa184cafcae040d7b77683d53f
Bretty Labash <brett.labash@gmail.com>
2019-05-09 22:26:18 -0700

    Removed some css for posts

--- frontend/components/dashboard/postForms/photo_post_form.jsx
+++ frontend/components/dashboard/postForms/photo_post_form.jsx
@@ -56,18 +56,6 @@ class PhotoPostForm extends React.Component {
                     {this.props.currentUser.username}
                 </button>
                 <form className="post-form-form" onSubmit={this.handleSubmit}>
-
-  <div className="box__input">
-    <input className="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
-    <label for="file"><strong>Choose a file</strong><span className="box__dragndrop"> or drag it here</span>.</label>
-    <button className="box__button" type="submit">Upload</button>
-  </div>
-  <div className="box__uploading">Uploading&hellip;</div>
-  <div className="box__success">Done!</div>
-  <div className="box__error">Error! <span></span>.</div>
-
-
-
                     <input type="file"
                         className="photo-post-form-file-input"
                         accept="image/*"
