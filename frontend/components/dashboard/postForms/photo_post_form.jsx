40ebb7e399039b68cd95033a66ec58aead649491
Bretty Labash <brett.labash@gmail.com>
2019-05-09 19:10:39 -0700

    photo post styling

--- frontend/components/dashboard/postForms/photo_post_form.jsx
+++ frontend/components/dashboard/postForms/photo_post_form.jsx
@@ -54,13 +54,25 @@ class PhotoPostForm extends React.Component {
                     {this.props.currentUser.username}
                 </button>
                 <form className="post-form-form" onSubmit={this.handleSubmit}>
+
+  <div className="box__input">
+    <input className="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
+    <label for="file"><strong>Choose a file</strong><span className="box__dragndrop"> or drag it here</span>.</label>
+    <button className="box__button" type="submit">Upload</button>
+  </div>
+  <div className="box__uploading">Uploading&hellip;</div>
+  <div className="box__success">Done!</div>
+  <div className="box__error">Error! <span></span>.</div>
+
+
+
                     <input type="file"
                         className="photo-post-form-file-input"
+                        accept="image/*"
                         onChange={this.handleFile.bind(this)}
                     />
                     <div className="post-form-caption">
-                        <input
-                            type="text"
+                        <textarea
                             className="photo-post-form-caption-input"
                             value={this.state.caption}
                             onChange={this.update("caption")}
